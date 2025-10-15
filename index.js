1d4fb70361f06dc3479655f532ff42a896a20405
kayatumk <166062885+kayatumk@users.noreply.github.com>
2025-10-15 17:28:51 +0100

    HTML 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http");
 const port = 8080; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type": "HTML" });
     res.end("<!doctype html><html><head><title>My Page</title></head><body><h1>Hello World</h1><p>This is my very small node.js application.</p></body></html>");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
