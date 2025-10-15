cac93e22ec8a057b38bcb354a1b42ac69ed08f5f
kayatumk <166062885+kayatumk@users.noreply.github.com>
2025-10-15 17:14:16 +0100

    HTML addition

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello My Friend!");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<!doctype html><html><head><title>My Page</title></head><body><h1>Hello My Friend!</h1><p>This is my very small node.js application.</p></body></html>");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
