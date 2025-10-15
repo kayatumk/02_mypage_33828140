935d64694a1a1dd51a0e4d43b65808d1dd7e37df
kayatumk <166062885+kayatumk@users.noreply.github.com>
2025-10-15 17:18:16 +0100

    Hello World

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8000;
 
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<!doctype html><html><head><title>My Page</title></head><body><h1>Hello My Friend!</h1><p>This is my very small node.js application.</p></body></html>");
+    res.end("Hello World");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
